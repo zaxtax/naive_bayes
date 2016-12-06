91cd68ed708387a9d02664e7ee67f861713edad8
Rob Zinkov <rob@zinkov.com>
2016-12-06 18:33:03 -0500

    Add simple summary script

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ echo "Running Baseline"
 # ./nb
 
 echo "Running Gibbs updates"
+
+echo "Lang,Docs,Categories,Vocab,Trial,Time" > gibbstimes.csv
 for i in {1..10}
 do
   ./NBHakaru 10 3 100 $i >> gibbstimes.csv
