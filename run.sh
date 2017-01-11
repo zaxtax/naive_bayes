4400dc0419d2c6a304fd0be87f0f086480ba37c5
Rob Zinkov <rob@zinkov.com>
2017-01-11 09:51:40 -0500

    Remove zsh-isms

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ echo "Running Baseline"
 echo "Running Gibbs updates"
 
 echo "Lang,Docs,Categories,Vocab,Trial,Time" > gibbstimes.csv
-for i in {1..10}
-do
+for i in `seq 1 10`; do
   ./NBHakaru 10 3 100 $i >> gibbstimes.csv
 done
