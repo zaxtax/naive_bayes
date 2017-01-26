acb699bec196566a5dba364b97b83ae28a9b1a80
Spenser Bauman <sabauma@gmail.com>
2017-01-26 14:13:39 -0500

    Fix size error in malloc call

--- gibbs.c
+++ gibbs.c
@@ -7,7 +7,7 @@ struct arrayProb* gibbsC_shim(struct arrayProb* topic_prior_b,
                 struct arrayNat* doc_f,
                 unsigned int docUpdate_g)
 {
-  struct arrayProb* res = (struct arrayProb*)malloc(sizeof(struct arrayProb*));
+  struct arrayProb* res = (struct arrayProb*)malloc(sizeof(struct arrayProb));
   *res = gibbsC(*topic_prior_b, *word_prior_c, *z_d, *w_e, *doc_f, docUpdate_g);
   return res;
 }
