3d3b4371276ce385be38c09e25a360013f77bba5
Spenser Bauman <sabauma@gmail.com>
2017-04-06 11:31:43 -0400

    Fix gibbs.c to be compatible with generated code

--- gibbs.c
+++ gibbs.c
@@ -1,13 +1,14 @@
 #include "nb_simp.c"
 
-struct array_prob* gibbsC_shim(struct array_prob* topic_prior_b,
-                struct array_prob* word_prior_c,
-                struct array_nat* z_d,
-                struct array_nat* w_e,
-                struct array_nat* doc_f,
-                unsigned int docUpdate_g)
+struct mdata_nat* gibbsC_shim(
+        struct array_prob* topic_prior_b,
+        struct array_prob* word_prior_c,
+        struct array_nat* z_d,
+        struct array_nat* w_e,
+        struct array_nat* doc_f,
+        unsigned int docUpdate_g)
 {
-  struct array_prob* res = (struct array_prob*)malloc(sizeof(struct array_prob));
+  struct mdata_nat* res = (struct mdata_nat*)malloc(sizeof(struct mdata_nat));
   *res = gibbsC(*topic_prior_b, *word_prior_c, *z_d, *w_e, *doc_f, docUpdate_g);
   return res;
 }
