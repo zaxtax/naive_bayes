2409a46fd625b7614b4c02cbb00a4fa04fae2f92
Rob Zinkov <rob@zinkov.com>
2017-04-27 16:09:16 -0400

    Gibbs sweeps matches r script

--- NaiveBayesMain.hs
+++ NaiveBayesMain.hs
@@ -1,21 +1,22 @@
 module Main where
 
+import           Control.Monad              (forever, replicateM, forM_)
 import qualified Data.ByteString.Char8 as B
-import News (getNews)
+import           Data.List                  (sort)
+import           Data.Number.LogFloat
+import qualified Data.Vector.Unboxed   as V
+import           Data.Vector.Unboxed        (Vector, (!))
+import           GibbsOptBucket             (prog)
+import           Language.Hakaru.Runtime.LogFloatPrelude
+                                            (unMeasure, prob_, categorical)
+import           News                       (getNews)
+import           System.Environment         (getArgs)
 import qualified System.Random.MWC as MWC
-import qualified Data.Vector.Unboxed as V
-import Data.Vector.Unboxed (Vector, (!))
-import Text.Printf (printf)
-import GibbsOptBucket (prog)
-import Language.Hakaru.Runtime.LogFloatPrelude (unMeasure)
-import Control.Monad (forever, replicateM, forM_)
-import Data.List (sort)
-import Data.Number.LogFloat
+import           Text.Printf                (printf)
 
 onesFrom :: Vector Int -> Vector LogFloat
 onesFrom v = V.replicate (V.maximum v + 1) 1
 
--- |Make it easier to draw a sample
 sample g m = do
   maybeX <- unMeasure m g
   case maybeX of
@@ -29,27 +30,46 @@ accuracy
 accuracy x y = V.sum z / (fromIntegral $ V.length x)
     where z = V.zipWith (\a b -> if a == b then 1 else 0) x y
 
+initializeZs
+    :: Int
+    -> Int
+    -> MWC.GenIO
+    -> IO (Vector Int)
+initializeZs testSize numTopics g =
+    V.replicateM testSize $
+      sample g (categorical $ V.replicate numTopics (prob_ 1))
+
 main = do
-  (words, docs, topics) <- getNews Nothing [0..]
-  g <- MWC.createSystemRandom
-  let 
-    zPrior = onesFrom topics
-    wPrior = onesFrom words
-    predict = prog zPrior wPrior topics words docs
-  printf "length zPrior == %d\n" (V.length zPrior)
-  printf "length wPrior == %d\n" (V.length wPrior)
-  printf "length words  == %d\n" (V.length words)
-  printf "length docs   == %d\n" (V.length docs)
-  printf "length topics == %d\n" (V.length topics)
-  --forM_ [0..(V.length topics - 1)] $ \i -> do
-  forM_ [0..10] $ \i -> do
-    --print $ V.map logFromLogFloat $ predict i
-    prediction <- sample g (predict i)
-    printf "%d %d\n" (topics ! i) prediction
-  -- print $ accuracy
-  --           topics
-  --           (V.map (V.maxIndex . predict) (V.generate (V.length topics) id))
-  -- replicateM 5 . withGen g (print . sort) $ do
-  --   pred <- predict
-  --   let p = pred $ V.fromList $ [0,1,7]
-  --   replicateM 100 p
+  args  <- getArgs
+  case length args == 2 of
+    False -> putStrLn "./naive-bayes <docsPerTopic≻ <trial>"
+    True  -> do
+         let [docsPerTopic, trail] = map read args :: [Int]
+         (words, docs, topics) <- getNews (Just docsPerTopic) [0..]
+         g <- MWC.createSystemRandom
+         let numTopics        = 20
+             trainTestSplit   = 0.9
+             testDocsPerTopic = floor (fromIntegral docsPerTopic *
+                                       (1.0 - trainTestSplit))
+             topicIndices     = getTopicIndices
+                                  testDocsPerTopic
+                                  docsPerTopic
+                                  topics
+                                  numTopics
+
+             zTrues  = V.map (topics !) topicIndices
+         zInits  <- initializeZs (testDocsPerTopic * numTopics) numTopics g
+         let topics' = V.update_ topics topicIndices zInits
+             zPrior  = onesFrom topics
+             wPrior  = onesFrom words
+             predict = prog zPrior wPrior topics' words docs
+         zPreds <- V.forM topicIndices $ \i -> sample g (predict i)
+         print zTrues
+         print zPreds
+         print $ accuracy zTrues zPreds
+
+getTopicIndices testDocsPerTopic docsPerTopic topics numTopics =
+    V.concatMap
+         (\i -> V.generate (testDocsPerTopic + 1) (+ i))
+         seq
+    where seq = V.generate numTopics (* docsPerTopic)
