4f173a6a509dbbffd35b0e2c4ca4de6b8599bea8
Rob Zinkov <rob@zinkov.com>
2017-04-28 23:47:01 -0400

    Make plots use denormed data format

--- plot.R
+++ plot.R
@@ -1,9 +1,10 @@
 library(ggplot2)
+library(tidyr)
 library(dplyr)
 library(extrafont)
 
 suppressMessages(loadfonts())
-dataFile <- "nbtimes.csv"
+dataFile <- "nbtimes_cleaned.csv"
 data <- read.csv(dataFile, header=T)
 
 pd <- position_dodge(0.1)
@@ -12,13 +13,27 @@ ci_calc <- function(sd, n, conf.lev = .95) {
     sd/sqrt(n) * qt(conf.lev/2 + .5, n - 1)
 }
 
+dataT <- data %>%
+    gather(Time.var, Time, Init.time, Update.time) %>%
+    group_by(System,DocSize,Time.var) %>%
+    summarise_each(funs(mean,
+                        sd,
+                        se=sd(.)/sqrt(n()),
+                        ci=ci_calc(sd(.),n())),
+                   Time) %>%
+    rename(Time=mean)
+
+dataT$System <- paste(dataT$System, dataT$Time.var, sep=".")
+dataT$Time.var <- NULL
+
 data <- data %>%
     group_by(System,DocSize) %>%
     summarise_each(funs(mean,
                         sd,
                         se=sd(.)/sqrt(n()),
                         ci=ci_calc(sd(.),n())),
-                   Acc, Time)
+                   Acc) %>%
+    rename(Acc=mean)
 
 theming <- theme_bw() +
         #ggtitle("Run times for Gaussian Mixture Model") +
@@ -43,30 +58,23 @@ theming <- theme_bw() +
         theme(legend.justification=c(0.02,1.0),
               legend.position=c(0.02,1.0))               # Position legend in bottom right
 
-pAcc <- ggplot(data, aes(x=DocSize, y=Acc_mean, colour=System, group=System)) +
-        geom_errorbar(aes(ymin=Acc_mean-Acc_se, ymax=Acc_mean+Acc_se),
+pAcc <- ggplot(data, aes(x=DocSize, y=Acc, colour=System, group=System)) +
+        geom_errorbar(aes(ymin=Acc-se, ymax=Acc+se),
                       colour="black", width=.1, position=pd) +
         geom_line(position=pd) +
         #geom_point(position=pd, size=3, shape=21, fill="white") + # 21 is filled circle
         xlab("Data size") +
         ylab("Accuracy") +
         geom_point(aes(shape=System), size=3) +
-        scale_shape(name="Inference method",    # Legend label, use darker colors
-                    breaks=c("JAGS_init", "JAGS"),
-                    labels=c("JAGS + initialization",
-                             "JAGS")) +
-        scale_color_hue(name="Inference method",    # Legend label, use darker colors
-                        breaks=c("JAGS_init", "JAGS"),
-                        labels=c("JAGS + initialization",
-                                 "JAGS"),
-                        l=40) +
+        scale_shape(name="System") +
+        scale_color_hue(name="System", l=40) +
         theming   
 
 ggsave("nb_plot_cm.pdf", pAcc) # width=4, height=3.5)
 embed_fonts("nb_plot_cm.pdf", outfile="nbplotacc.pdf")
 
-pT <-   ggplot(data, aes(x=DocSize, y=Time_mean, colour=System, group=System)) +
-        geom_errorbar(aes(ymin=Time_mean-Time_se, ymax=Time_mean+Time_se),
+pT <-   ggplot(dataT, aes(x=DocSize, y=Time, colour=System, group=System)) +
+        geom_errorbar(aes(ymin=Time-se, ymax=Time+se),
                     colour="black", width=.1, position=pd) +
         geom_line(position=pd) +
         #geom_point(position=pd, size=3, shape=21, fill="white") + # 21 is filled circle
@@ -74,7 +82,7 @@ pT <-   ggplot(data, aes(x=DocSize, y=Time_mean, colour=System, group=System)) +
         ylab("Run time (secs)") +
         geom_point(aes(shape=System), size=3) +
         scale_shape(name="Inference method",    # Legend label, use darker colors
-                    breaks=c("JAGS_init", "JAGS"),
+                    breaks=c("JAGS.Init.time", "JAGS.Update.time"),
                     labels=c("JAGS + initialization",
                              "JAGS")) +
         scale_color_hue(name="Inference method",    # Legend label, use darker colors
