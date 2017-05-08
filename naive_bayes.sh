50388a934fba1218fb4bad91dd839095c0ed78c1
Rob Zinkov <rob@zinkov.com>
2017-05-08 00:22:35 -0400

    Add Hakaru into naive_bayes script

--- naive_bayes.sh
+++ naive_bayes.sh
@@ -10,3 +10,15 @@ start=`date +%s.%N`
 ./naive_bayes.R $1 $2
 end=`date +%s.%N`
 echo "$end - $start" | bc -l
+
+if [ -f nbsimp.hk ]; then
+    rm nbsimp.hk nbsimp2.hk GibbsOptBucket.hs
+fi
+start=`date +%s.%N`
+simplify naive_bayes_gibbs.hk > nbsimp.hk
+perl unsample.pl nbsimp.hk > nbsimp2.hk
+summary nbsimp2.hk -o GibbsOptBucket.hs -M GibbsOptBucket --logfloat-prelude
+ghc -v0 -O2 NaiveBayesMain.hs -o NaiveBayes
+./NaiveBayes $1 $2
+end=`date +%s.%N`
+echo "$end - $start" | bc -l
